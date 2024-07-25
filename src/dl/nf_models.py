f65edbc4668199dd910c550a0931848c5c03278b
Manu Joseph <manujosephv@gmail.com>
2024-07-25 07:52:49 +0530

    Added probabilistic forecasting notebooks and src files

---
+++ src/dl/nf_models.py
@@ -0,0 +1,38 @@
+from neuralforecast.common._base_windows import BaseWindows
+import torch
+import torch.nn as nn
+from typing import Tuple
+
+class MovingAvg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+
+    def __init__(self, kernel_size, stride):
+        super(MovingAvg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1].repeat(1, (self.kernel_size - 1) // 2)
+        end = x[:, -1:].repeat(1, (self.kernel_size - 1) // 2)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x)
+        return x
+
+
+class SeriesDecomp(nn.Module):
+    """
+    Series decomposition block
+    """
+
+    def __init__(self, kernel_size):
+        super(SeriesDecomp, self).__init__()
+        self.MovingAvg = MovingAvg(kernel_size, stride=1)
+
+    def forward(self, x):
+        moving_mean = self.MovingAvg(x)
+        res = x - moving_mean
+        return res, moving_mean
+
