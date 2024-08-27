0454bc8378f51d8a82c1f17936df29050d56f283
Manu Joseph <manujosephv@gmail.com>
2024-08-28 05:25:29 +0530

    Added prob fc notebooks and src fles

--- src/dl/nf_models.py
+++ src/dl/nf_models.py
@@ -2,6 +2,7 @@ from neuralforecast.common._base_windows import BaseWindows
 import torch
 import torch.nn as nn
 from typing import Tuple
+from neuralforecast.losses.pytorch import MAE
 
 class MovingAvg(nn.Module):
     """
@@ -36,3 +37,108 @@ class SeriesDecomp(nn.Module):
         res = x - moving_mean
         return res, moving_mean
 
+
+class DNonLinear(BaseWindows):
+    def __init__(
+        self,
+        # Inhereted hyperparameters with no defaults
+        h,
+        input_size,
+        # Model specific hyperparameters
+        moving_avg_window=3,
+        dropout=0.1,
+        # Inhereted hyperparameters with defaults
+        exclude_insample_y=False,
+        loss=MAE(),
+        valid_loss=None,
+        max_steps: int = 1000,
+        learning_rate: float = 1e-3,
+        num_lr_decays: int = -1,
+        early_stop_patience_steps: int = -1,
+        val_check_steps: int = 100,
+        batch_size: int = 32,
+        valid_batch_size: int = None,
+        windows_batch_size=1024,
+        inference_windows_batch_size=-1,
+        start_padding_enabled: bool = False,
+        step_size: int = 1,
+        scaler_type: str = "identity",
+        random_seed: int = 1,
+        num_workers_loader: int = 0,
+        drop_last_loader: bool = False,
+        **trainer_kwargs,
+    ):
+        if moving_avg_window % 2 == 0:
+            raise Exception("moving_avg_window should be uneven")
+        super(DNonLinear, self).__init__(
+            h=h,
+            input_size=input_size,
+            exclude_insample_y=exclude_insample_y,
+            loss=loss,
+            valid_loss=valid_loss,
+            max_steps=max_steps,
+            val_check_steps=val_check_steps,
+            batch_size=batch_size,
+            valid_batch_size=valid_batch_size,
+            windows_batch_size=windows_batch_size,
+            inference_windows_batch_size=inference_windows_batch_size,
+            start_padding_enabled=start_padding_enabled,
+            step_size=step_size,
+            num_lr_decays=num_lr_decays,
+            learning_rate=learning_rate,
+            early_stop_patience_steps=early_stop_patience_steps,
+            scaler_type=scaler_type,
+            random_seed=random_seed,
+            drop_last_loader=drop_last_loader,
+            num_workers_loader=num_workers_loader,
+            **trainer_kwargs,
+        )
+        # Model specific hyperparameters
+        self.moving_avg_window = moving_avg_window
+        self.dropout = dropout
+        # Model components
+        self.decomp = SeriesDecomp(self.moving_avg_window)
+        self.non_linear_block = nn.Sequential(
+            nn.Dropout(self.dropout),
+            nn.Linear(self.input_size, 100),
+            nn.ReLU(),
+            nn.Dropout(self.dropout),
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Dropout(self.dropout),
+            nn.Linear(100, self.h),
+        )
+        self.linear_trend = nn.Sequential(
+            nn.Dropout(self.dropout),
+            nn.Linear(self.input_size, self.h),
+        )
+        self.seasonality = nn.Sequential(
+            nn.Dropout(self.dropout),
+            nn.Linear(self.input_size, self.h),
+        )
+
+    def forward(self, windows_batch):
+        # Parse windows_batch
+        insample_y = windows_batch[
+            "insample_y"
+        ].clone()  # --> (batch_size, input_size)
+        seasonal_init, trend_init = self.decomp(
+            insample_y
+        )  # --> (batch_size, input_size)
+        # Non-linear block
+        non_linear_part = self.non_linear_block(
+            insample_y
+        )  # --> (batch_size, horizon)
+        # Linear trend block
+        trend_part = self.linear_trend(trend_init)  # --> (batch_size, horizon)
+        # Seasonality block
+        seasonal_part = self.seasonality(
+            seasonal_init
+        )  # --> (batch_size, horizon)
+        # Combine the components
+        forecast = (
+            trend_part + seasonal_part + non_linear_part
+        )  # --> (batch_size, horizon)
+        # Map the forecast to the domain of the target
+        forecast = self.loss.domain_map(forecast)
+        return forecast
--- src/utils/ts_utils.py
+++ src/utils/ts_utils.py
@@ -3,7 +3,10 @@ from functools import partial
 from src.decomposition.seasonal import _detrend
 from typing import Callable, Dict, List
 
-def make_stationary(x: np.ndarray, method: str="detrend", detrend_kwargs:dict={}):
+
+def make_stationary(
+    x: np.ndarray, method: str = "detrend", detrend_kwargs: dict = {}
+):
     """Utility to make time series stationary
 
     Args:
@@ -11,31 +14,36 @@ def make_stationary(x: np.ndarray, method: str="detrend", detrend_kwargs:dict={}
         method (str, optional): {"detrend","logdiff"}. Defaults to "detrend".
         detrend_kwargs (dict, optional): These kwargs will be passed on to the detrend method
     """
-    if method=="detrend":
+    if method == "detrend":
         detrend_kwargs["return_trend"] = True
         stationary, trend = _detrend(x, **detrend_kwargs)
+
         def inverse_transform(st, trend):
-            return st+trend
+            return st + trend
+
         return stationary, partial(inverse_transform, trend=trend)
     elif method == "logdiff":
-        stationary = np.log(x[:-1]/x[1:])
+        stationary = np.log(x[:-1] / x[1:])
+
         def inverse_transform(st, x):
             _x = np.exp(st)
-            return _x*x[1:]
+            return _x * x[1:]
+
         return stationary, partial(inverse_transform, x=x)
 
-#from darts import TimeSeries
-#from darts.metrics.metrics import _get_values_or_raise
-#from darts.metrics import metrics as dart_metrics
+
+# from darts import TimeSeries
+# from darts.metrics.metrics import _get_values_or_raise
+# from darts.metrics import metrics as dart_metrics
 from datasetsforecast.losses import *
 from typing import Optional, Tuple, Union, Sequence, Callable, cast
 from src.utils.data_utils import is_datetime_dtypes
 import pandas as pd
 
 
-
-def _remove_nan_union(array_a: np.ndarray,
-                      array_b: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+def _remove_nan_union(
+    array_a: np.ndarray, array_b: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
     """
     Returns the two inputs arrays where all elements are deleted that have an index that corresponds to
     a NaN value in either of the two input arrays.
@@ -44,12 +52,14 @@ def _remove_nan_union(array_a: np.ndarray,
     isnan_mask = np.logical_or(np.isnan(array_a), np.isnan(array_b))
     return np.delete(array_a, isnan_mask), np.delete(array_b, isnan_mask)
 
+
 def _zero_to_nan(series: Union[pd.Series, "pl.Expr"]) -> Union[pd.Series, "pl.Expr"]:
     if isinstance(series, pd.Series):
         res = series.replace(0, np.nan)
     else:
         res = pl.when(series == 0).then(float("nan")).otherwise(series.abs())
     return res
+
 
 def forecast_bias_NIXTLA(
     df: pd.DataFrame,
@@ -60,8 +70,8 @@ def forecast_bias_NIXTLA(
     """Forecast Bias (FB)
 
     Forecast bias measures the percentage of over or under forecasting by the model.
-    If the bias is negative, it means the model is underforecasting, meaning the 
-    forecast is less than actuals. If the bias is positive, it means the model 
+    If the bias is negative, it means the model is underforecasting, meaning the
+    forecast is less than actuals. If the bias is positive, it means the model
     is overforecasting, meaning the forecast is greater than the actuals."""
 
     # Select only the necessary columns before grouping
@@ -74,9 +84,14 @@ def forecast_bias_NIXTLA(
     # Calculate the forecast bias
     res = (
         temp[models]
-        .sub(temp[target_col], axis=0)  # Subtract the target column from the models predictions
-        .div(_zero_to_nan(temp[target_col].abs()), axis=0)  # Normalize by the absolute values of the target column
-        .fillna(0)*100  # Fill NA with zero
+        .sub(
+            temp[target_col], axis=0
+        )  # Subtract the target column from the models predictions
+        .div(
+            _zero_to_nan(temp[target_col].abs()), axis=0
+        )  # Normalize by the absolute values of the target column
+        .fillna(0)
+        * 100  # Fill NA with zero
     )
 
     # Set the index name and reset the index to make 'id_col' a column again
@@ -85,7 +100,9 @@ def forecast_bias_NIXTLA(
 
     return res
 
+
 # recreated DARTS function to remove dependency
+
 
 def _get_values(
     vals: np.ndarray, stochastic_quantile: Optional[float] = 0.5
@@ -105,15 +122,17 @@ def _get_values(
     return out
 
 
-def forecast_bias(actual_series: Union[ np.ndarray],
-        pred_series: Union[ np.ndarray],
-        intersect: bool = True,
-        *,
-        reduction: Callable[[np.ndarray], float] = np.mean,
-        inter_reduction: Callable[[np.ndarray], Union[float, np.ndarray]] = lambda x: x,
-        n_jobs: int = 1,
-        verbose: bool = False) -> Union[float, np.ndarray]:
-    """ Forecast Bias (FB).
+def forecast_bias(
+    actual_series: Union[np.ndarray],
+    pred_series: Union[np.ndarray],
+    intersect: bool = True,
+    *,
+    reduction: Callable[[np.ndarray], float] = np.mean,
+    inter_reduction: Callable[[np.ndarray], Union[float, np.ndarray]] = lambda x: x,
+    n_jobs: int = 1,
+    verbose: bool = False,
+) -> Union[float, np.ndarray]:
+    """Forecast Bias (FB).
 
     Given a time series of actual values :math:`y_t` and a time series of predicted values :math:`\\hat{y}_t`
     both of length :math:`T`, it is a percentage value computed as
@@ -158,105 +177,127 @@ def forecast_bias(actual_series: Union[ np.ndarray],
     float
         The Forecast Bias (OPE)
     """
-    assert type(actual_series) is type(pred_series), "actual_series and pred_series should be of same type."
+    assert type(actual_series) is type(
+        pred_series
+    ), "actual_series and pred_series should be of same type."
     if isinstance(actual_series, np.ndarray):
         y_true, y_pred = actual_series, pred_series
     else:
         y_true = actual_series
         y_pred = pred_series
     #     y_true, y_pred = _get_values_or_raise(actual_series, pred_series, intersect)
-    #y_true, y_pred = _remove_nan_union(y_true, y_pred)
+    # y_true, y_pred = _remove_nan_union(y_true, y_pred)
     y_true_sum, y_pred_sum = np.sum(y_true), np.sum(y_pred)
     # raise_if_not(y_true_sum > 0, 'The series of actual value cannot sum to zero when computing OPE.', logger)
-    return ((y_true_sum - y_pred_sum) / y_true_sum) * 100.
+    return ((y_true_sum - y_pred_sum) / y_true_sum) * 100.0
+
 
 def cast_to_series(df):
-    is_pd_dataframe = isinstance(df, pd.DataFrame)    
-    if is_pd_dataframe: 
-        if df.shape[1]==1:
+    is_pd_dataframe = isinstance(df, pd.DataFrame)
+    if is_pd_dataframe:
+        if df.shape[1] == 1:
             df = df.squeeze()
         else:
-            raise ValueError("Dataframes with more than one columns cannot be converted to pd.Series")
+            raise ValueError(
+                "Dataframes with more than one columns cannot be converted to pd.Series"
+            )
     return df
 
-def metrics_adapter(metric_func, actual_series,
-        pred_series,
-        insample = None,
-        m: Optional[int] = 1,
-        intersect: bool = True,
-        reduction: Callable[[np.ndarray], float] = np.mean,
-        inter_reduction: Callable[[np.ndarray], Union[float, np.ndarray]] = lambda x: x,
-        n_jobs: int = 1,
-        verbose: bool = False):
+
+def metrics_adapter(
+    metric_func,
+    actual_series,
+    pred_series,
+    insample=None,
+    m: Optional[int] = 1,
+    intersect: bool = True,
+    reduction: Callable[[np.ndarray], float] = np.mean,
+    inter_reduction: Callable[[np.ndarray], Union[float, np.ndarray]] = lambda x: x,
+    n_jobs: int = 1,
+    verbose: bool = False,
+):
     """
     This function is designed to adapt various time series metrics functions to ensure they are compatible with the expected input types, handling both numpy arrays and pandas Series. It standardizes input time series data before passing them to the specified metric function, providing a flexible interface for time series analysis.
     Parameters
     ----------
-    metric_func (Callable): 
+    metric_func (Callable):
         The metric function to be applied. It should accept at least two arguments: the actual series and the predicted series. Optional parameters like `insample` for MASE can also be passed if supported by the function.
-    actual_series (np.ndarray or pd.Series): 
+    actual_series (np.ndarray or pd.Series):
         The actual values of the time series.
-    pred_series (np.ndarray or pd.Series): 
+    pred_series (np.ndarray or pd.Series):
         The predicted values of the time series.
-    insample (np.ndarray or pd.Series, optional): 
+    insample (np.ndarray or pd.Series, optional):
         The in-sample data needed for some metrics like MASE. Default is None.
-    m (Optional[int]): 
+    m (Optional[int]):
         An optional integer parameter that might be needed for some metrics, like seasonal MASE, to specify the seasonality. Default is 1.
-    intersect (bool): 
+    intersect (bool):
         If True, aligns the actual and predicted series on their intersection based on their time indices before applying the metric. Default is True.
-    reduction (Callable[[np.ndarray], float]): 
+    reduction (Callable[[np.ndarray], float]):
         A function to reduce the computed metric across dimensions in case of multivariate time series. Default is `np.mean`.
-    inter_reduction (Callable[[np.ndarray], Union[float, np.ndarray]]): 
+    inter_reduction (Callable[[np.ndarray], Union[float, np.ndarray]]):
         A function to reduce or transform the intermediate computed metrics across multiple time series or ensemble predictions. Defaults to an identity function, which returns the metrics as-is.
-    n_jobs (int): 
+    n_jobs (int):
         Number of parallel jobs to run when the metric function supports parallel execution. Defaults to 1 (sequential execution).
-    verbose (bool): 
+    verbose (bool):
         If True, additional details regarding the computation process may be printed. Useful for debugging or tracking computation progress. Defaults to False.
     Returns
     ----------
-    ValueError: 
+    ValueError:
         If the time index is required (e.g., for MASE) and not available in the series inputs.
-    AssertionError: 
+    AssertionError:
         If the input series types do not match or expected conditions for parameters are not met.
- 
+
     """
-    actual_series, pred_series = cast_to_series(actual_series), cast_to_series(pred_series)
+    actual_series, pred_series = cast_to_series(actual_series), cast_to_series(
+        pred_series
+    )
     if insample is not None:
         insample = cast_to_series(insample)
-    assert type(actual_series) is type(pred_series), f"actual_series({type(actual_series)}) and pred_series({type(pred_series)}) should be of same type."
+    assert type(actual_series) is type(
+        pred_series
+    ), f"actual_series({type(actual_series)}) and pred_series({type(pred_series)}) should be of same type."
     if insample is not None:
-        assert type(actual_series) is type(insample), "actual_series and insample should be of same type."
+        assert type(actual_series) is type(
+            insample
+        ), "actual_series and insample should be of same type."
     is_nd_array = isinstance(actual_series, np.ndarray)
     is_pd_series = isinstance(actual_series, pd.Series)
-    
+
     if is_pd_series:
-        is_datetime_index = is_datetime_dtypes(actual_series.index) and is_datetime_dtypes(pred_series.index)
+        is_datetime_index = is_datetime_dtypes(
+            actual_series.index
+        ) and is_datetime_dtypes(pred_series.index)
         if insample is not None:
-            is_datetime_index = is_datetime_index and is_datetime_dtypes(insample.index)
+            is_datetime_index = is_datetime_index and is_datetime_dtypes(
+                insample.index
+            )
     else:
         is_datetime_index = False
     if metric_func.__name__ == "mase":
         if not is_datetime_index:
             raise ValueError("MASE needs pandas Series with datetime index as inputs")
-    
+
     if metric_func.__name__ == "mase":
-        #return metric_func(actual_series=actual_series, pred_series=pred_series, insample=insample, m=m, intersect=intersect, reduction=reduction, inter_reduction=inter_reduction, n_jobs=n_jobs, verbose=verbose)
+        # return metric_func(actual_series=actual_series, pred_series=pred_series, insample=insample, m=m, intersect=intersect, reduction=reduction, inter_reduction=inter_reduction, n_jobs=n_jobs, verbose=verbose)
         return metric_func(actual_series, pred_series, insample)
 
     else:
-        #return metric_func(actual_series=actual_series, pred_series=pred_series, intersect=intersect, reduction=reduction, inter_reduction=inter_reduction, n_jobs=n_jobs, verbose=verbose)
+        # return metric_func(actual_series=actual_series, pred_series=pred_series, intersect=intersect, reduction=reduction, inter_reduction=inter_reduction, n_jobs=n_jobs, verbose=verbose)
         return metric_func(actual_series, pred_series)
 
+
 def mae(actuals, predictions):
-    return np.nanmean(np.abs(actuals-predictions))
+    return np.nanmean(np.abs(actuals - predictions))
+
 
 def mse(actuals, predictions):
-    return np.nanmean(np.power(actuals-predictions, 2))
+    return np.nanmean(np.power(actuals - predictions, 2))
+
 
 def mase(actuals, predictions, insample):
     """
     Calculate the Mean Absolute Scaled Error (MASE).
-    
+
     Parameters:
     actuals : np.ndarray
         Actual observed values corresponding to the predictions.
@@ -271,87 +312,73 @@ def mase(actuals, predictions, insample):
     """
     # Calculate MAE of predictions
     mae_predictions = np.nanmean(np.abs(actuals - predictions))
-    
+
     # Shift the insample data to create a simple naive forecast
     naive_forecast = np.roll(insample, 1)
     # Assuming the first element is not a valid forecast
-    naive_forecast[0] = np.nan 
-    
+    naive_forecast[0] = np.nan
+
     # Calculate MAE of the naive forecast
     mae_naive = np.nanmean(np.abs(insample - naive_forecast))
-    
+
     # Calculate MASE
     mase_value = mae_predictions / mae_naive
     return mase_value
 
+
 def forecast_bias_aggregate(actuals, predictions):
-    return 100*(np.nansum(predictions)-np.nansum(actuals))/np.nansum(actuals)
+    return 100 * (np.nansum(predictions) - np.nansum(actuals)) / np.nansum(actuals)
 
-# def rmsse(
-#     actual_series,
-#     pred_series,
-#     insample,
-#     m = 1,
-#     intersect = True,
-#     *,
-#     reduction = np.mean,
-# ):
 
-    # def _multivariate_mase(
-    #     actual_series,
-    #     pred_series,
-    #     insample,
-    #     m,
-    #     intersect,
-    #     reduction,
-    # ):
+# Average Length that wors with utilsforecast (Nixtla)
+def average_length(
+    df: pd.DataFrame,
+    models: List[str],
+    level: int,
+    id_col: str = "unique_id",
+    target_col: str = "y",
+) -> pd.DataFrame:
+    """Average Length of y_hat_lo and y_hat_hi.
 
-    #     assert actual_series.width == pred_series.width, "The two TimeSeries instances must have the same width."
-        
-    #     assert actual_series.width == insample.width, "The insample TimeSeries must have the same width as the other series."
-        
-    #     assert insample.end_time() + insample.freq == pred_series.start_time(), "The pred_series must be the forecast of the insample series"
+    Parameters
+    ----------
+    df : pandas or polars DataFrame
+        Input dataframe with id, times, actuals and predictions.
+    models : list of str
+        Columns that identify the models predictions.
+    level : int
+        Confidence level used for intervals.
+    id_col : str (default='unique_id')
+        Column that identifies each serie.
+    target_col : str (default='y')
+        Column that contains the target.
 
-    #     insample_ = (
-    #         insample.quantile_timeseries(quantile=0.5)
-    #         if insample.is_stochastic
-    #         else insample
-    #     )
+    Returns
+    -------
+    pandas or polars Dataframe
+        dataframe with one row per id and one column per model.
+    """
+    if isinstance(df, pd.DataFrame):
+        out = np.empty((df.shape[0], len(models)))
+        for j, model in enumerate(models):
+            out[:, j] = df[f"{model}-hi-{level}"] - df[f"{model}-lo-{level}"]
+        res = (
+            pd.DataFrame(out, columns=models, index=df.index)
+            .groupby(df[id_col], observed=True)
+            .mean()
+        )
+        res.index.name = id_col
+        res = res.reset_index()
+    else:
+        raise NotImplementedError("Only pandas DataFrames are supported for now.")
+    return res
 
-    #     value_list = []
-    #     for i in range(actual_series.width):
-    #         # old implementation of mase on univariate TimeSeries
-    #         y_true, y_hat = _get_values_or_raise(
-    #             actual_series.univariate_component(i),
-    #             pred_series.univariate_component(i),
-    #             intersect,
-    #             remove_nan_union=False,
-    #         )
 
-    #         x_t = insample_.univariate_component(i).values()
-    #         errors = np.square(y_true - y_hat)
-    #         scale = np.mean(np.square(x_t[m:] - x_t[:-m]))
-    #         assert not np.isclose(scale, 0), "cannot use MASE with periodical signals"
-    #         value_list.append(np.sqrt(np.mean(errors / scale)))
+def level_to_quantiles(l):
+    if l > 1:
+        l = l / 100
+    return [round(q, 2) for q in [0.5 - l * 0.5, 0.5 + l * 0.5]]
 
-    #     return reduction(value_list)
 
-    # if isinstance(actual_series
-    #               #, TimeSeries
-    #               ):
-    #     #assert isinstance(pred_series, TimeSeries), "Expecting pred_series to be TimeSeries"
-    #     #assert isinstance(insample, TimeSeries), "Expecting insample to be TimeSeries"
-    #     return _multivariate_mase(
-    #         actual_series=actual_series,
-    #         pred_series=pred_series,
-    #         insample=insample,
-    #         m=m,
-    #         intersect=intersect,
-    #         reduction=reduction,
-    #     )
-    # else:
-    #     raise(
-    #         ValueError(
-    #             "Input type not supported, only TimeSeries is accepted."
-    #         )
-    #     )
+def error_rate_to_quantiles(alpha):
+    return [round(q, 2) for q in [alpha / 2, 1 - alpha / 2]]
