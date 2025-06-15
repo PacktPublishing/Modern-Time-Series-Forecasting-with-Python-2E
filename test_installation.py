496566021aff50d40d7071f21933846e14de2a2b
Jeff Tackes <tackes@gmail.com>
2025-06-15 13:49:22 -0500

    #36 Updated test_installation and yaml for plotly <5.4

--- test_installation.py
+++ test_installation.py
@@ -28,6 +28,7 @@ import seaborn
 import neuralforecast
 import pytorch_lightning as pl
 import src
+
 #from pytorch_forecasting import TimeSeriesDataSet
 #from pytorch_forecasting.data import GroupNormalizer
 #from pytorch_forecasting.metrics import RMSE, MAE
